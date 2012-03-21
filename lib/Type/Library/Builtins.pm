03a278a4eda514acba113c1dc2e7cb706e9c28df
Dave Rolsky <autarch@urth.org>
2012-03-20 20:31:47 -0500

    Add HashRef builtin type

--- lib/Type/Library/Builtins.pm
+++ lib/Type/Library/Builtins.pm
@@ -182,4 +182,25 @@ declare(
     },
 );
 
+declare(
+    'HashRef',
+    type_class => 'Type::Constraint::Parameterizable',
+    parent     => t('Ref'),
+    inline     => sub { 'ref(' . $_[1] . q{) eq 'HASH'} },
+    parameterized_inline_generator => sub {
+        my $self      = shift;
+        my $parameter = shift;
+        my $val       = shift;
+
+        return
+              'do {'
+            . 'my $value = '
+            . $val . ';'
+            . q{ref($value) eq 'HASH' }
+            . '&& List::MoreUtils::all {'
+            . $parameter->_inline_check('$_') . ' } '
+            . 'values %{$value}' . '}';
+    },
+);
+
 1;
