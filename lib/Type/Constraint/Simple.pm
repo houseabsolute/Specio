3d61fa035aa83fa6ca163475626d3665260f691b
Dave Rolsky <autarch@urth.org>
2012-05-12 22:53:08 -0500

    Start docs for simple type

--- lib/Type/Constraint/Simple.pm
+++ lib/Type/Constraint/Simple.pm
@@ -11,3 +11,11 @@ with 'Type::Constraint::Role::Interface';
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+# ABSTRACT: Class for simple (non-parameterized or specialized) types
+
+__END__
+
+=head1 SYNOPSIS
+
+  my $tc = Type::Constraint::Simple
