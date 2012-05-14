5668c802f61b44c4ac3dcca81025d66c5488602e
Dave Rolsky <autarch@urth.org>
2012-05-14 12:24:09 -0500

    More docs

--- lib/Type/Constraint/AnyIsa.pm
+++ lib/Type/Constraint/AnyIsa.pm
@@ -45,3 +45,45 @@ has '+message_generator' => (
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+# ABSTRACT: A class for constraints which require a class name or an object that inherit from a specific class
+
+__END__
+
+=head1 SYNOPSIS
+
+  my $type = Type::Constraint::AnyIsa->new(...);
+  print $type->class();
+
+=head1 DESCRIPTION
+
+This is a specialized type constraint class for types which require a class
+name or an object that inherit from a specific class.
+
+=head1 API
+
+This class provides all of the same methods as L<Type::Constraint::Simple>,
+with a few differences:
+
+=head2 Type::Constraint::AnyIsa->new( ... )
+
+The C<parent> parameter is ignored if it passed, as it is always set to the
+C<Defined> type.
+
+The C<inline_generator> and C<constraint> parameters are also ignored. This
+class provides its own default inline generator subroutine reference.
+
+This class overrides the C<message_generator> default if none is provided.
+
+Finally, this class requires an additional parameter, C<class>. This must be a
+single class name.
+
+=head2 $any_isa->class()
+
+Returns the class name passed to the constructor.
+
+=head1 ROLES
+
+This class does the L<Type::Constraint::Role::IsaType>,
+L<Type::Constraint::Role::Interface>, L<Type::Role::Inlinable>, and
+L<MooseX::Clone> roles.
