3d8c87f64a1e503d930508d55d426e39ea882952
Dave Rolsky <autarch@urth.org>
2012-05-14 17:57:31 -0500

    Add docs for Type::Exception

--- lib/Type/Exception.pm
+++ lib/Type/Exception.pm
@@ -24,3 +24,31 @@ has value => (
 __PACKAGE__->meta()->make_immutable( inline_constructor => 0 );
 
 1;
+
+# ABSTRACT: A Throwable::Error subclass for type constraint failures
+
+=head1 DESCRIPTION
+
+  use Try::Tiny;
+
+  try {
+      $type->validate_or_die($value);
+  }
+  catch {
+      if ( $_->isa('Type::Exception') ) {
+          print $_->message(), "\n";
+          print $_->type()->name(), "\n";
+          print $_->value(), "\n";
+      }
+  };
+
+=head1 DESCRIPTION
+
+This is a subclass of L<Throwable::Error> which adds a few additional
+attributes specific to type constraint failures.
+
+=head1 API
+
+The two attributes it adds are C<type> and C<value>, both of which are
+required. The C<type> must be a L<Type::Constraint::Role::Interface> object,
+and the C<value> can be anything (including C<undef>).
