d73ef56991b312542419109ec1c7d1cd8751e0e9
Dave Rolsky <autarch@urth.org>
2012-05-14 12:20:00 -0500

    Docs for AnyCan class

--- lib/Type/Constraint/AnyCan.pm
+++ lib/Type/Constraint/AnyCan.pm
@@ -46,3 +46,47 @@ has '+message_generator' => (
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+# ABSTRACT: A class for constraints which require a class name or object with a set of methods
+
+__END__
+
+=head1 SYNOPSIS
+
+  my $type = Type::Constraint::AnyCan->new(...);
+  print $_, "\n" for @{ $type->methods() };
+
+=head1 DESCRIPTION
+
+This is a specialized type constraint class for types which require a class
+name or object with a defined set of methods.
+
+=head1 API
+
+This class provides all of the same methods as L<Type::Constraint::Simple>,
+with a few differences:
+
+=head2 Type::Constraint::AnyCan->new( ... )
+
+The C<parent> parameter is ignored if it passed, as it is always set to the
+C<Defined> type.
+
+The C<inline_generator> and C<constraint> parameters are also ignored. This
+class provides its own default inline generator subroutine reference.
+
+This class overrides the C<message_generator> default if none is provided.
+
+Finally, this class requires an additional parameter, C<methods>. This must be
+an array reference of method names which the constraint requires. You can also
+pass a single string and it will be converted to an array reference
+internally.
+
+=head2 $any_can->methods()
+
+Returns an array reference containing the methods this constraint requires.
+
+=head1 ROLES
+
+This class does the L<Type::Constraint::Role::CanType>,
+L<Type::Constraint::Role::Interface>, L<Type::Role::Inlinable>, and
+L<MooseX::Clone> roles.
