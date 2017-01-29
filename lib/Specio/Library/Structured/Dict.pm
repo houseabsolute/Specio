9c70cde6b604740bd1e4dbde6fed59ef6a26dfaa
Dave Rolsky <autarch@urth.org>
2017-01-28 22:12:42 -0600

    Add alpha implementation of structured types
    The syntax for defininig these types and the guts may change in the future,
    but this is a good enough start to release for others to look at.

---
+++ lib/Specio/Library/Structured/Dict.pm
@@ -0,0 +1,128 @@
+package Specio::Library::Structured::Dict;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.34';
+
+use Carp qw( confess );
+use List::Util ();
+use Scalar::Util qw( blessed );
+use Specio::Library::Builtins;
+use Specio::TypeChecks qw( does_role );
+
+my $hashref = t('HashRef');
+
+sub parent {$hashref}
+
+## no critic (Subroutines::ProhibitUnusedPrivateSubroutines)
+sub _inline {
+    $hashref->_inline_check( $_[1] );
+}
+
+sub _parameterization_args_builder {
+    my $self = shift;
+    my $args = shift;
+
+    for my $p ( ( $args->{slurpy} || () ), values %{ $args->{kv} } ) {
+        my $type;
+        if ( blessed($p) ) {
+            $type = $p;
+        }
+        else {
+            if ( ref $p eq 'HASH' && $p->{optional} ) {
+                $type = $p->{optional};
+            }
+            else {
+                confess
+                    'Can only pass types, optional types, and slurpy types when defining a Dict';
+            }
+        }
+
+        does_role( $type, 'Specio::Constraint::Role::Interface' )
+            or confess
+            'All parameters passed to ->parameterize must be objects which do the Specio::Constraint::Role::Interface role';
+
+        confess
+            'All parameters passed to ->parameterize must be inlinable constraints'
+            unless $type->can_be_inlined;
+    }
+
+    return %{$args};
+}
+
+sub _name_builder {
+    my $self = shift;
+    my $p    = shift;
+
+    ## no critic (Subroutines::ProtectPrivateSubs)
+    my @kv;
+    for my $k ( sort keys %{ $p->{kv} } ) {
+        my $v = $p->{kv}{$k};
+        if ( blessed($v) ) {
+            push @kv, "$k => " . $self->_name_or_anon($v);
+        }
+        elsif ( $v->{optional} ) {
+            push @kv,
+                "$k => " . $self->_name_or_anon( $v->{optional} ) . '?';
+        }
+    }
+
+    if ( $p->{slurpy} ) {
+        push @kv, $self->_name_or_anon( $p->{slurpy} ) . '...';
+    }
+
+    return 'Dict{ ' . ( join ', ', @kv ) . ' }';
+}
+
+sub _structured_inline_generator {
+    my $self = shift;
+    my $val  = shift;
+    my %args = @_;
+
+    my @code = sprintf( '( %s )', $hashref->_inline_check($val) );
+
+    for my $k ( sort keys %{ $args{kv} } ) {
+        my $p = $args{kv}{$k};
+        my $access = sprintf( '%s->{%s}', $val, B::perlstring($k) );
+
+        if ( !blessed($p) ) {
+            my $type = $p->{optional};
+
+            push @code,
+                sprintf(
+                '( exists %s ? ( %s ) : 1 )',
+                $access, $type->_inline_check($access)
+                );
+        }
+        else {
+            push @code, sprintf( '( %s )', $p->_inline_check($access) );
+        }
+    }
+
+    if ( $args{slurpy} ) {
+        my $check
+            = '( do { my %%_____known_____ = map { $_ => 1 } ( %s ); List::Util::all { %s } grep { ! $_____known_____{$_} } sort keys %%{ %s } } )';
+        push @code,
+            sprintf(
+            $check,
+            ( join ', ', map { B::perlstring($_) } keys %{ $args{kv} } ),
+            $args{slurpy}->_inline_check( sprintf( '%s->{$_}', $val ) ),
+            $val,
+            );
+    }
+
+    return '( ' . ( join ' && ', @code ) . ' )';
+}
+
+1;
+
+# ABSTRACT: Guts of Dict structured type
+
+__END__
+
+=for Pod::Coverage .*
+
+=head1 DESCRIPTION
+
+There are no user facing parts here.
---
+++ lib/Specio/Library/Structured/Map.pm
@@ -0,0 +1,85 @@
+package Specio::Library::Structured::Map;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.34';
+
+use Carp qw( confess );
+use List::Util ();
+use Specio::Library::Builtins;
+use Specio::TypeChecks qw( does_role );
+
+my $hashref = t('HashRef');
+
+sub parent {$hashref}
+
+## no critic (Subroutines::ProhibitUnusedPrivateSubroutines)
+sub _inline {
+    $hashref->_inline_check( $_[1] );
+}
+
+sub _parameterization_args_builder {
+    my $self = shift;
+    my $args = shift;
+
+    for my $k (qw( key value )) {
+        does_role(
+            $args->{$k},
+            'Specio::Constraint::Role::Interface'
+            )
+            or confess
+            qq{The "$k" parameter passed to ->parameterize must be one or more objects which do the Specio::Constraint::Role::Interface role};
+
+        confess
+            qq{The "$k" parameter passed to ->parameterize must be an inlinable constraint}
+            unless $args->{$k}->can_be_inlined;
+    }
+    return map { $_ => $args->{$_} } qw( key value );
+}
+
+sub _name_builder {
+    my $self = shift;
+    my $p    = shift;
+
+    ## no critic (Subroutines::ProtectPrivateSubs)
+    return
+          'Map{ '
+        . $self->_name_or_anon( $p->{key} ) . ' => '
+        . $self->_name_or_anon( $p->{value} ) . ' }';
+}
+
+sub _structured_inline_generator {
+    my $self = shift;
+    my $val  = shift;
+    my %args = @_;
+
+    my $code = <<'EOF';
+(
+    ( %s )
+    && ( List::Util::all { %s } keys %%{ %s } )
+    && ( List::Util::all { %s } values %%{ %s } )
+)
+EOF
+
+    return sprintf(
+        $code,
+        $hashref->_inline_check($val),
+        $args{key}->inline_check('$_'),
+        $val,
+        $args{value}->inline_check('$_'),
+        $val,
+    );
+}
+
+1;
+
+# ABSTRACT: Guts of Map structured type
+
+__END__
+
+=for Pod::Coverage .*
+
+=head1 DESCRIPTION
+
+There are no user facing parts here.
